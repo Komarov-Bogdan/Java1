4e641900ab5d9cb6b0e180598f7f0c27d18f9009
Komarov-Bogdan <komarovb.it@gmail.com>
2024-04-17 23:09:29 +0300

    SimpleCalculator

---
+++ src/main/java/org/example/homework3/SimpleCalculator.java
@@ -0,0 +1,23 @@
+package org.example.homework3;
+
+public class SimpleCalculator
+{
+    public static void main(String[] args)
+    {
+        int a = 7;
+        double b = 19;
+        //sum
+        double sum = a + b;
+        System.out.println("Sum is: " + sum );
+        //subtraction
+        double subtraction = a - b;
+        System.out.println("Subtraction is: " + subtraction );
+        //multiplication
+        double multiplication = a * b;
+        System.out.println("Multiplication is: " + multiplication );
+        //division
+        double division = a / b;
+        System.out.println("Division is: " + division );
+
+    }
+}
